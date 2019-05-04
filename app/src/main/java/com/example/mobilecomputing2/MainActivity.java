5b82088b068c2cf9be1476fd1d7d41e1e4aa29e2
Justin Kissling <st107873@stud.uni-stuttgart.de>
2019-05-04 21:45:02 +0200

    Added reading for accelerometer

--- app/src/main/java/com/example/mobilecomputing2/MainActivity.java
+++ app/src/main/java/com/example/mobilecomputing2/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.mobilecomputing2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private TextView textAccel;
+
+    private SensorManager sensorManager;
+
+    private Sensor accelSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
+        initUI();
+    }
+
+    private void initUI() {
+        textAccel = (TextView) findViewById(R.id.textAccel);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        textAccel.setText(String.valueOf(event.values[0]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
