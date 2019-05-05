ff98e73d4076c1f947dc73e200c7e7b176a51ce7
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-05-05 20:31:29 +0200

    Added work in progress gps logics

--- app/src/main/java/com/example/mobilecomputing2/MainActivity.java
+++ app/src/main/java/com/example/mobilecomputing2/MainActivity.java
@@ -1,39 +1,53 @@
 package com.example.mobilecomputing2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class MainActivity extends Activity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private TextView textAccel;
+    private TextView textGPS;
 
     private SensorManager sensorManager;
 
     private Sensor accelSensor;
 
+    private BroadcastReceiver broadcastReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
+
+        checkPermissions();
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-
-        initUI();
     }
 
     private void initUI() {
+        textGPS = findViewById(R.id.textGPS);
+        textGPS.setText("GPS coordinates: unknown");
+
         textAccel = (TextView) findViewById(R.id.textAccel);
     }
 
@@ -50,11 +64,64 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onResume() {
         super.onResume();
         sensorManager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        if (broadcastReceiver == null) {
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    textGPS.setText("GPS coordinates: " + intent.getExtras().get("coordinates"));
+                }
+            };
+        }
+
+        registerReceiver(broadcastReceiver, new IntentFilter("current_coordinates"));
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (broadcastReceiver != null) {
+            unregisterReceiver(broadcastReceiver);
+        }
+
+        Intent i = new Intent(this, SensorService.class);
+        stopService(i);
+    }
+
+    /**
+     * This method checks and grants all necessary permissions
+     */
+    private void checkPermissions() {
+        // Check and grant internet permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 10);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 10: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent i = new Intent(this, SensorService.class);
+                    startService(i);
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/mobilecomputing2/SensorService.java
@@ -0,0 +1,70 @@
+package com.example.mobilecomputing2;
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class SensorService extends Service {
+
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onCreate() {
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Intent i = new Intent("current_coordinates");
+                i.putExtra("coordinates", "Lng: " + location.getLongitude() + "; Lat: " + location.getLatitude());
+                sendBroadcast(i);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 0, locationListener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(locationManager != null) {
+            locationManager.removeUpdates(locationListener);
+        }
+    }
+}
